381ced787259960d183386f604283beea3bddc1d
user013 <mostimportantemailbox@gmail.com>
2021-08-25 12:50:57 +0300

    Made delay on-the-go

--- src/main/java/com/bell/thyme/config/Delayer.java
+++ src/main/java/com/bell/thyme/config/Delayer.java
@@ -1,40 +1,43 @@
 package com.bell.thyme.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-import javax.annotation.PostConstruct;
-
-@Component
 public class Delayer extends Thread {
 
-    @Value("${mock.firstDelay}")
     public long delayFromProperties;
-
-    @Value("${mock.firstFrequency}")
     private long frequency;
-
     public static long actualDelay;
-
-    @PostConstruct
-    public void init(){
-        actualDelay = delayFromProperties;
-    }
 
     @Override
     public void run() {
-
         while(Delayer.currentThread().isAlive())   {
-
             init();
-
-            //нельзя ставить Thread.sleep внутрь цикла! Но Thread.wait - подойдет
-           frequencyDelayed();
+            frequencyDelayed();
         }
     }
 
+    public void init() {
+
+        File delayProperties = new File("C:\\Users\\user013\\Desktop\\DelayProperties.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(delayProperties);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] parameters = line.split(",");
+            delayFromProperties = Long.parseLong(parameters[1]);
+            frequency = Long.parseLong(parameters[2]);
+        }
+        scanner.close();
+        actualDelay = delayFromProperties;
+    }
+
     public void frequencyDelayed() {
-        //надо вызывать метод wait из синхронизированного блока
         synchronized (Thread.currentThread()) {
             try {
                 Thread.currentThread().wait(frequency);
@@ -44,7 +47,6 @@ public class Delayer extends Thread {
         }
     }
 
-    @PostConstruct
     public static long getActualDelay(){
         return actualDelay;
     }
